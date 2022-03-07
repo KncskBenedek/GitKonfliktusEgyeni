3d109fc4316abcbb523a1afdef190c7a5f5036c0
KncskBenedek <95653038+KncskBenedek@users.noreply.github.com>
2022-03-07 14:47:08 +0100

    3. nev deklarálása

--- src/konfliktus/Konfliktus.java
+++ src/konfliktus/Konfliktus.java
@@ -5,10 +5,10 @@ public class Konfliktus {
 
     
     public static void main(String[] args) {
-
+        int i = 7;
         String nev1 = "Koncsik Benedek";
         String nev2 = "X.Y.";
-
+        int nev3;
     }
     
 }
